12ad9a7f4b50d0f162ef670be55d62937e9cfec9
Martijn Jansen <martijnjansen91@gmail.com>
2023-10-31 09:08:55 +0100

    initial commit

---
+++ src/main/java/com/runescape/info/RunescapesiteApplication.java
@@ -0,0 +1,13 @@
+package com.runescape.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunescapesiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunescapesiteApplication.class, args);
+	}
+
+}
