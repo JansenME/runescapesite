4813edd74b00ce6528898d59e92db7f6b37d1435
Martijn Jansen <martijnjansen91@gmail.com>
2023-11-17 11:28:25 +0100

    remove unused variable

--- src/main/java/com/maulsinc/runescape/controller/ErrorController.java
+++ src/main/java/com/maulsinc/runescape/controller/ErrorController.java
@@ -2,15 +2,12 @@ package com.maulsinc.runescape.controller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
-    @Value("${app.version:unknown}")
-    String version;
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
